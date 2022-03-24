84d5de3d52f395fefb4c464c8f07c3ed0ec88a06
Matthias Gernand <mgernand@users.noreply.github.com>
2022-03-24 21:35:36 +0100

    Updated packages. (#5)

--- tests/Fluxera.Extensions.Hosting.UnitTests/TestApplicationLifetime.cs
+++ tests/Fluxera.Extensions.Hosting.UnitTests/TestApplicationLifetime.cs
@@ -1,8 +1,10 @@
 ﻿namespace Fluxera.Extensions.Hosting.UnitTests
 {
+	using System.Diagnostics.CodeAnalysis;
 	using System.Threading;
 	using Microsoft.Extensions.Hosting;
 
+	[SuppressMessage("ReSharper", "UnassignedGetOnlyAutoProperty")]
 	public class TestApplicationLifetime : IHostApplicationLifetime
 	{
 		/// <inheritdoc />
