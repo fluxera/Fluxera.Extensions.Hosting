8d6638c9112db6b402fa42a149b7d7f2c55c9347
Matthias Gernand <matthias.gernand@fluxera.com>
2022-04-10 22:14:26 +0200

    Removed web module and added extension method to add the action context accessor.

---
+++ src/Fluxera.Extensions.Hosting.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Fluxera.Extensions.Hosting
+{
+	using Fluxera.Guards;
+	using JetBrains.Annotations;
+	using Microsoft.AspNetCore.Mvc.Infrastructure;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.DependencyInjection.Extensions;
+
+	/// <summary>
+	///     Extension methods for the <see cref="IServiceCollection" /> type.
+	/// </summary>
+	[PublicAPI]
+	public static class ServiceCollectionExtensions
+	{
+		/// <summary>
+		///     Adds a default implementation for the <see cref="IActionContextAccessor" /> service.
+		/// </summary>
+		/// <param name="services">The <see cref="IServiceCollection" />.</param>
+		/// <returns>The service collection.</returns>
+		public static IServiceCollection AddActionContextAccessor(this IServiceCollection services)
+		{
+			Guard.Against.Null(services, nameof(services));
+
+			services.TryAddSingleton<IActionContextAccessor, ActionContextAccessor>();
+			return services;
+		}
+	}
+}
