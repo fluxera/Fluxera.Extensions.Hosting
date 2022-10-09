3f72441e08e21c5721a4cbc6f46e44e8c232de19
Matthias Gernand <matthias.gernand@fluxera.com>
2022-10-09 19:01:53 +0200

    Added testing environment extension helper.

---
+++ src/Fluxera.Extensions.Hosting/HostEnvironmentExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Fluxera.Extensions.Hosting
+{
+	using Fluxera.Guards;
+	using JetBrains.Annotations;
+	using Microsoft.Extensions.Hosting;
+
+	/// <summary>
+	///     Extension methods for the <see cref="IHostEnvironment" /> type.
+	/// </summary>
+	[PublicAPI]
+	public static class HostEnvironmentExtensions
+	{
+		private static readonly string Testing = "Testing";
+
+		/// <summary>
+		///     Checks if the current host environment name is <see cref="Testing" />.
+		/// </summary>
+		/// <param name="hostEnvironment">An instance of <see cref="IHostEnvironment" />.</param>
+		/// <returns>True if the environment name is <see cref="Testing" />, otherwise false.</returns>
+		public static bool IsTesting(this IHostEnvironment hostEnvironment)
+		{
+			hostEnvironment = Guard.Against.Null(hostEnvironment);
+
+			return hostEnvironment.IsEnvironment(Testing);
+		}
+	}
+}
