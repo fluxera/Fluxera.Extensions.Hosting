eebbfaa94fd185760c97a3dd7e45be364092f54b
Matthias Gernand <mgernand@users.noreply.github.com>
2022-10-12 22:27:07 +0200

    Updated packages. (#26)

--- samples/MauiApp1/MauiApp1Module.cs
+++ samples/MauiApp1/MauiApp1Module.cs
@@ -3,13 +3,19 @@
 	using Fluxera.Extensions.Hosting;
 	using Fluxera.Extensions.Hosting.Modules;
 
-	public class MauiApp1Module : ConfigureServicesModule
+	public class MauiApp1Module : ConfigureApplicationModule
 	{
 		/// <inheritdoc />
 		public override void ConfigureServices(IServiceConfigurationContext context)
 		{
-			context.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+			context.Services.AddSingleton(Connectivity.Current);
 			context.Services.AddSingleton<MainPage>();
+		}
+
+		/// <inheritdoc />
+		public override void OnApplicationShutdown(IApplicationShutdownContext context)
+		{
+			base.OnApplicationShutdown(context);
 		}
 	}
 }
