b110dfae632628cd8b76bfc0247352edceb9c15d
Matthias Gernand <mgernand@users.noreply.github.com>
2022-11-24 12:51:44 +0100

    Allow generic modules. (#31)

---
+++ tests/Fluxera.Extensions.Hosting.UnitTests/GenericApplicationModule.cs
@@ -0,0 +1,10 @@
+﻿namespace Fluxera.Extensions.Hosting.UnitTests
+{
+	using Fluxera.Extensions.Hosting.Modules;
+	using JetBrains.Annotations;
+
+	[PublicAPI]
+	public class GenericApplicationModule<T> : ConfigureApplicationModule
+	{
+	}
+}
