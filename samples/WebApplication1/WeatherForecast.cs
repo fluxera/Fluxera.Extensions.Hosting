7ebe7265510cb8efc5846df37a96242f370faa72
Matthias Gernand <mgernand@users.noreply.github.com>
2023-06-21 14:01:16 +0200

    Updated packages. (#45)

--- samples/WebApplication1/WeatherForecast.cs
+++ samples/WebApplication1/WeatherForecast.cs
@@ -10,6 +10,6 @@ namespace WebApplication1
 
 		public int TemperatureF => 32 + (int)(this.TemperatureC / 0.5556);
 
-		public string? Summary { get; set; }
+		public string Summary { get; set; }
 	}
 }
